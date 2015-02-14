fbcdfec5e408927caf4c178b53dea3b9c848441c
Shubh Vachher <shubhvachher@gmail.com>
2015-02-15 03:57:34 +0530

    sigh

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-import py2exe
-import sys
-#setup(console=["install.py"])
-#setup(console=["sender.py"])
-#setup(console=["run.py"])
-name = raw_input("Enter file name")
-setup(console=[name])
